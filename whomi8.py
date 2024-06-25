3cc125699aef19a77ca61b6627e6f4bb76f49a6b
nomankarim8 <karimnoman97@gmail.com>
2024-06-25 06:35:22 -0700

    commit

---
+++ whomi8.py
@@ -0,0 +1,21 @@
+
+x = 'y'
+y = 'G'
+z = 'a'
+
+nam2 = 'siddik'
+
+nam = input("Tor nam lek : ")
+
+if(nam2 == nam):
+    while True:
+        print("Siddik tor nanir jamij")
+        
+print("hea vai ami", nam)
+print("Ar ami akta", y, z, x)
+
+fuck = 1
+
+for fuck in range(10):
+    print(nam, "Gay")
+    fuck += 1
