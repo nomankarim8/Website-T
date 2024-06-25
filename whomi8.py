2b16f965254cfc91c83ab83ff45bc24cdbd15fe2
nomankarim8 <karimnoman97@gmmail.com>
2024-06-25 19:46:33 +0800

    commit

--- whomi8.py
+++ whomi8.py
@@ -9,7 +9,7 @@ nam = input("type our name  : ")
 
 if(nam2 == nam):
     while True:
-        print("whomi8 is")
+        print("whomi8 is your grand")
         
 print("hea vai ami", nam)
 print("Ar ami akta", y, z, x)
