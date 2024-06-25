4b14f14780931e5a600b66d39bc7a0e48f74fb92
nomankarim8 <karimnoman97@gmail.com>
2024-06-25 19:53:30 +0800

    commit

--- whomi8.py
+++ whomi8.py
@@ -12,7 +12,7 @@ if(nam2 == nam):
         print("whomi8 is your grandmother's husband")
         
 print("yes brother i am ", nam)
-print("As i ", y, z, x)
+print("As i am ", y, z, x)
 
 fuck = 1
 
